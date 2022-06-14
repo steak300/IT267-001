cbf76569dbb87f58a14206f37379536645a65a5d
steak300 <steaksiripun@gmail.com>
2022-06-14 14:30:39 +0700

    update

--- week2/animal.py
+++ week2/animal.py
@@ -17,6 +17,31 @@ class Animal:
         print(f"Colour: {self.colour}")
         print(f"Age: {self.age}")
         
+    def __del__(self):
+        print(f'Object was destoryed')
 #create object
-ula = Animal()
-ula.new_animal
+if __name__ == "__main__":
+    Animal.animal = "Fish"
+    ula = Animal()
+    ula.animal = "DOG"
+    ula.new_animal("Ula","Scottish","White",1)
+    ula.print_detail()
+    
+    drac = Animal()
+    drac.new_animal("Drac","Scottish","White",1)
+    drac.print_detail()
+    drac.breed = "Catfish"
+    drac.print_detail()
+    
+    Animal.print_detail(ula)#ula.print_detail()
+    
+    #เรียกดู class variableทั้งหมด
+    print(f'{Animal.__dict__}')
+    
+    print(f'{ula.__dict__}')
+    
+    peter = Animal()
+    peter.new_animal("Peter","Parrot","Green Yellow Red",2)
+    #add new  
+    peter.legs = 2
+    print(f'{peter.__dict__}')
